74cf6957e739867f519c6a478121f7888065649f
ghccaixeta <gustavohcaixeta@gmail.com>
2023-09-02 18:01:19 -0300

    Extrai dados exceto lançamentos

--- readpdf.py
+++ readpdf.py
@@ -2,6 +2,64 @@ import pdfplumber
 
 inputPDF = pdfplumber.open('recibo-pagamento.pdf')
 
+class Contracheque:
+    def __init__(
+        self,
+        nomeFantasia, 
+        cnpj, 
+        centroDeCusto,
+        tipoLancamento,
+        tipoJornada,
+        periodo,
+        codigoFuncionario,
+        nomeFuncionario,
+        cargoFuncionario,
+        cbo,
+        departamento,
+        filial,
+        admissaoFuncionario,
+        lancamentos,
+        totalVencimentos,
+        totalDescontos,
+        valorLiquido,
+        salarioBase,
+        salarioINSS,
+        baseCalcFGTS,
+        fgtsMes,
+        baseCalcIRRF,
+        faixaIRRF
+        ):
+        self.nomeFantasia = nomeFantasia, 
+        self.cnpj = cnpj, 
+        self.centroDeCust = centroDeCusto,
+        self.tipoLancament = tipoLancamento,
+        self.period = periodo,
+        self.codigoFuncionari = codigoFuncionario,
+        self.nomeFuncionari = nomeFuncionario,
+        self.cargoFuncionari = cargoFuncionario,
+        self.cb = cbo,
+        self.departament = departamento,
+        self.filia = filial,
+        self.admissaoFuncionari = admissaoFuncionario,
+        self.lancamento = lancamentos,
+        self.totalVenciment = totalVencimentos
+        self.totalDescont = totalDescontos
+        self.valorLiquid = valorLiquido,
+        self.salarioBas = salarioBase,
+        self.salarioINS = salarioINSS,
+        self.baseCalcFGT = baseCalcFGTS,
+        self.fgtsMe = fgtsMes,
+        self.baseCalcIRR = baseCalcIRRF,
+        self.faixaIR = faixaIRRF
+
+def getLineContent(fullText, searchText):
+    
+    for item in fullText:
+        if searchText in item:
+            return item
+
+    
+    return ""
 
 pageNumber = 0;
 
@@ -29,7 +87,32 @@ for item in conteudoArray:
     else:
         print("Não foi possível localizar o rodapé.")
 
+contracheques = [];
 
 for item in contrachequesArray:
-    print(item)
-    print("----------------------------------------------------")
+    lines = item.splitlines()
+    nomeFantasia = lines[0]
+    linha02 = lines[1]
+    cnpj = linha02.split(" ")[1]
+    linha03 = lines[2]
+    tipoJornada = linha03.split(" ")[:-3][0]
+    periodo = ' '.join(linha03.split(" ")[-3:])
+
+    vencimentos = getLineContent(lines, 'ataD')
+
+    totalVencimentos = vencimentos.split(" ")[0]
+    totalDescontos = vencimentos.split(" ")[1]
+
+    linesSplit = lines[-1].split(" ")
+
+    salarioBase = linesSplit[0]
+    salarioINSS = linesSplit[1]
+    baseCalcFGTS = linesSplit[2]
+    fgtsMes = linesSplit[3]
+    baseCalcIRRF = linesSplit[4]
+    faixaIRRF = linesSplit[5]
+
+    print(salarioBase, salarioINSS,baseCalcFGTS,fgtsMes,baseCalcIRRF,faixaIRRF)
+
+    
+    
--- test.py
+++ test.py
@@ -1,50 +1,16 @@
-# Seu texto completo
+# Seu texto
 texto = """
-TOUCH COMUNICACAO INTEGRADA LTDA.
-CNPJ: 08.968.873/0001-02 CC: Centro de Custo Adiantamento
-Mensalista Julho de 2023
 Código Nome do Funcionário CBO Departamento Filial
-3 GISLAINE DE VARGAS TRENZ 521110 1 1
-VENDEDOR PLENO Admissão: 15/08/2022
+8 EDIMARA CAMILA ROLIM DE MOURA 521110 1 1
+VENDEDOR PLENO Admissão: 08/03/2023
 Código Descrição Referência Vencimentos Descontos
 980 ADIANTAMENTO SALARIAL 40,00 1.200,00 .obicer
-etsen
-oiránoicnuF
-adanimircsid
-od
-arutanissA
-adiuqíl
-aicnâtropmi
-a
-odibecer
-ret
-oralceD
-_______/____/____
-Total de Vencimentos Total de Descontos
-1.200,00 0,00 ataD
-
-Valor Líquido 1.200,00
-Salário Base Sal. Contr. INSS Base Cálc. FGTS F.G.T.S do Mês Base Cálc. IRRF Faixa IRRF
-3.000,00 0,00 0,00 0,00 0,00 0,00
-TOUCH COMUNICACAO INTEGRADA LTDA.
-CNPJ: 08.968.873/0001-02 CC: Centro de Custo Adiantamento
 """
 
-# Encontre a posição da linha que contém "Faixa IRRF"
-posicao_faixa_irrf = texto.find("IRRF")
-print(posicao_faixa_irrf)
+# Divida o texto em linhas usando splitlines()
+linhas = texto.splitlines()
 
-# Encontre a posição da primeira linha após "Faixa IRRF"
-linha_1_apos_faixa_irrf = texto.find("\n", posicao_faixa_irrf + 1)
-print(linha_1_apos_faixa_irrf)
-# Encontre a posição da segunda linha após "Faixa IRRF"
-linha_2_apos_faixa_irrf = texto.find("\n", linha_1_apos_faixa_irrf + 1)
-print(linha_2_apos_faixa_irrf)
-
-# Verifique se ambas as posições foram encontradas
-if posicao_faixa_irrf != -1 and linha_2_apos_faixa_irrf != -1:
-    # Pegue o conteúdo entre a primeira linha e a segunda linha abaixo de "Faixa IRRF"
-    conteudo = texto[:linha_2_apos_faixa_irrf].strip()
-    print(conteudo)
-else:
-    print("Linha com 'Faixa IRRF' ou segunda linha abaixo não foi encontrada.")
+# Itere pelas linhas para encontrar a linha que contém ".obicer" e imprima-a
+for linha in linhas:
+    if ".obicer" in linha:
+        print(linha)
